9907e12266e09e6afcf18008b90898692c83a181
Sarhoz Mahmoud <sarhozmahmoud@student.tudelft.nl>
2025-06-06 20:44:59 +0200

    remove bn and new weights

--- Software/main.py
+++ Software/main.py
@@ -119,15 +119,19 @@ class TinyBinCNN(nn.Module):
         super().__init__()
         self.act = BinaryActivation.apply
         self.conv1 = BinaryConv2d(1, 8, 3, padding=0)
-        self.bn1   = nn.BatchNorm2d(8)
+        # self.bn1   = nn.BatchNorm2d(8)
         self.conv2 = BinaryConv2d(8, 16, 3, padding=0)
-        self.bn2   = nn.BatchNorm2d(16)
+        # self.bn2   = nn.BatchNorm2d(16)
         self.pool  = nn.MaxPool2d(2,2)
         self.fc    = BinaryLinear(5*5*16, 10)
     def forward(self, x):
-        x = self.act(self.bn1(self.conv1(x)))
+        # x = self.act(self.bn1(self.conv1(x)))
+        # x = self.pool(x)
+        # x = self.act(self.bn2(self.conv2(x)))
+        # x = self.pool(x)
+        x = self.act(self.conv1(x))
         x = self.pool(x)
-        x = self.act(self.bn2(self.conv2(x)))
+        x = self.act(self.conv2(x))
         x = self.pool(x)
         x = x.view(x.size(0), -1)
         return self.fc(x)
@@ -281,7 +285,7 @@ def main():
     if args.search:
         best_bs, best_opt, best_lr = hyperparam_search(device, train_ds, val_ds)
     else:
-        best_bs, best_opt, best_lr = 64, 'RMSprop', 5e-3
+        best_bs, best_opt, best_lr = 64, 'SGD', 1
     flush_print(f"Step 2: Hyperparameters -> BS={best_bs}, OPT={best_opt}, LR={best_lr}")
 
     train_loader = DataLoader(train_ds, batch_size=best_bs, shuffle=True)
